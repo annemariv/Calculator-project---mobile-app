41dd55a5a2d43056cbf6b09a2c2f58cb02cf57d0
annemariv <annemari.velling@gmail.com>
2025-09-08 16:17:26 +0300

    Add new view and calculator page

--- Skycalc/SciCalc/SciCalc/App.xaml.cs
+++ Skycalc/SciCalc/SciCalc/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace SciCalc
+﻿using SciCalc.Views;
+
+namespace SciCalc
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            MainPage = new CalculatorPage();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
---
+++ Skycalc/SciCalc/SciCalc/Views/CalculatorPage.xaml.cs
@@ -0,0 +1,12 @@
+using SciCalc.ViewModels;
+
+namespace SciCalc.Views;
+
+public partial class CalculatorPage : ContentPage
+{
+	public CalculatorPage()
+	{
+		InitializeComponent();
+        this.BindingContext = new CalculatorPageViewModel();
+    }
+}
