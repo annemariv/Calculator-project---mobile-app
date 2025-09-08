efce0b0bce65649ed6da7ce20a9abb56b51bc23e
annemariv <annemari.velling@gmail.com>
2025-09-08 13:29:59 +0300

    New font added

---
+++ Skycalc/SciCalc/SciCalc/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SciCalc
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("Cairo-ExtraLight.ttf", "RegularFont");
+                    fonts.AddFont("Cairo-Light.ttf", "LightFont");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
