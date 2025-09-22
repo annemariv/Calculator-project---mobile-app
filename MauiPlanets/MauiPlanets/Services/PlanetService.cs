3972ec2709cfc5c456379e184180dc4a43f7a280
annemariv <annemari.velling@gmail.com>
2025-09-22 15:25:56 +0300

    Add PlanetPage

---
+++ MauiPlanets/MauiPlanets/Services/PlanetService.cs
@@ -0,0 +1,42 @@
+﻿using MauiPlanets.Models;
+
+
+namespace MauiPlanets.Services
+{
+    internal class PlanetService
+    {
+        private static List<Planet> planets = new()
+        {
+            new()
+            {
+                Name = "Mercury",
+                Subtitle = "The smallets planet",
+                HeroImage = "mercury.png",
+                Description = "Mercury is the first planet from the" +
+                " Sun and the smallerst in the Solar system. In English, it is named  " +
+                "after the ancient..Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. " +
+                "In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.",
+                AccentColorStart = Color.FromArgb("#353535"),
+                AccentColorEnd = Color.FromArgb("#8d9098"),
+                Images = new()
+                {
+                    "https://cdn.theatlantic.com/thumbor/D15rQggf6357X1-u6VpTD2N1yQE=/0x27:1041x613/976x549/media/img/mt/2017/04/MercuryImage/original.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/73_carousel_mercury_2.jpg",
+                    "https://solarsystem.nasa.gov/system/feature_items/images/75_mercury_carousel_1.jpg"
+                }
+            }
+        };
+        public static List<Planet> GetFeaturedPlanets()
+        {
+            var random = new Random();
+            var randomizePlanets = planets
+                .OrderBy(item => random.Next());
+
+            return randomizePlanets
+                .Take(2)
+                .ToList();
+        }
+        public static List<Planet> GetAllPlanets()
+            => planets;
+    }
+}
--- MauiPlanets/MauiPlanets/Views/PlanetPage.xaml.cs
+++ MauiPlanets/MauiPlanets/Views/PlanetPage.xaml.cs
@@ -1,9 +1,35 @@
+using MauiPlanets.Services;
+
 namespace MauiPlanets.Views;
 
 public partial class PlanetPage : ContentPage
 {
-	public PlanetPage()
+	private const uint AnimationDuration = 800u;
+
+    public PlanetPage()
 	{
 		InitializeComponent();
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+		//lstPopularPlanets.ItemSource = PlanetService.GetFeaturedPlanets();
+  //      lstAllPlanets.ItemSource = PlanetService.GetAllPlanets();
+    }
+
+    async void ProfilePic_Clicked(System.Object sender, System.EventArgs e)
+    {
+        // fire-and-forget animatsioon (ei oota lõppu)
+        _ = MainContentGrid.TranslateTo(
+            -this.Width * 0.5,
+            this.Height * 0.1,
+            AnimationDuration,
+            Easing.CubicIn);
+
+        // ootab, kuni ScaleTo lõpeb
+        await MainContentGrid.ScaleTo(0.8, AnimationDuration);
+    }
+
 }
